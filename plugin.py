e6c0e0ae26b84c7afcfcef20d1ee4ed9935ad759
einfall <einfall@nix.de>
2013-03-09 17:23:03 +0100

    page blättern mit channel+/-

--- plugin.py
+++ plugin.py
@@ -803,16 +803,19 @@ class haupt_Screen_Wall(Screen, ConfigListScreen):
 		for x in range(1,len(self.plugin_liste)+1):
 			skincontent += "<widget name=\"zeile" + str(x) + "\" position=\"" + str(posx) + "," + str(posy) + "\" size=\"150,80\" zPosition=\"1\" transparent=\"0\" alphatest=\"blend\" />"
 			posx += 155
-			#posx += 175
-			if x == 8 or x == 16 or x == 24 or x == 32 or x == 40:
+			if x == 8 or x == 16 or x == 24 or x == 32 or x == 48 or x == 56 or x == 64 or x == 72:
 				posx = 20
 				posy += 85
+			elif x == 40 or x == 80:
+				posx = 20
+				posy = 210
 
 		print skincontent
 				
 		self.skin_dump = ""
 		self.skin_dump += "<screen name=\"MediaPortal\" position=\"0,0\" size=\"1280,720\" flags=\"wfNoBorder\">"
 		self.skin_dump += "<widget name=\"name\" position=\"20,150\" size=\"900,50\" foregroundColor=\"#00ffffff\" backgroundColor=\"#26181d20\" transparent=\"1\" font=\"Regular;28\" valign=\"top\" halign=\"center\" />"
+		self.skin_dump += "<widget name=\"page\" position=\"1100,680\" size=\"100,40\" foregroundColor=\"#00ffffff\" backgroundColor=\"#26181d20\" transparent=\"1\" font=\"Regular;28\" valign=\"top\" halign=\"center\" />"
 		self.skin_dump += "<ePixmap position=\"0,0\" size=\"1280,720\" zPosition=\"-1\" pixmap=\"/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/tec/images/mpback.png\" />"
 		self.skin_dump += "<widget source=\"session.VideoPicture\" render=\"Pig\" position=\"913,15\" size=\"320,180\" zPosition=\"3\" backgroundColor=\"transparent\" />"
 		self.skin_dump += "<widget name=\"frame\" position=\"20,210\" size=\"150,80\" pixmap=\"/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/icons_wall/selektor_blau_24bit.png\" zPosition=\"2\" transparent=\"0\" alphatest=\"blend\" />"
@@ -823,17 +826,20 @@ class haupt_Screen_Wall(Screen, ConfigListScreen):
 		
 		Screen.__init__(self, session)
 		
-		self["actions"]  = ActionMap(["OkCancelActions", "ShortcutActions", "WizardActions", "ColorActions", "SetupActions", "NumberActions", "MenuActions"], {
+		self["actions"]  = ActionMap(["OkCancelActions", "ShortcutActions", "WizardActions", "ColorActions", "SetupActions", "NumberActions", "MenuActions", "EPGSelectActions"], {
 			"ok"    : self.keyOK,
 			"up"    : self.keyUp,
 			"down"  : self.keyDown,
 			"cancel": self.keyCancel,
 			"left"  : self.keyLeft,
 			"right" : self.keyRight,
+			"nextBouquet" :	self.page_next,
+			"prevBouquet" :	self.page_back,
 			"menu" : self.keySetup
 		}, -1)
 		
 		self['name'] = Label("Plugin Auswahl")
+		self['page'] = Label("")
 		self["frame"] = MovingPixmap()
 		for x in range(1,len(self.plugin_liste)+1):
 			self["zeile"+str(x)] = Pixmap()
@@ -848,7 +854,6 @@ class haupt_Screen_Wall(Screen, ConfigListScreen):
 		for x in range(1,len(self.plugin_liste)+1):
 			postername = self.plugin_liste[int(x)-1][1]
 			poster_path = "%s/%s.png" % ("/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/icons_wall", postername)
-			#poster_path = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/icons_wall"
 			if not fileExists(poster_path):
 				poster_path = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/icons_wall/no_icon.png"
 
@@ -886,6 +891,8 @@ class haupt_Screen_Wall(Screen, ConfigListScreen):
 					self.mainlist.append(list_dummy)
 					
 		print self.mainlist
+		pageinfo = "%s / %s" % (self.select_list+1, len(self.mainlist))
+		self['page'].setText(pageinfo)
 					
 	def move_selector(self):
 		plugin_name = self.plugin_liste[int(self.selektor_index)-1][0]
@@ -1074,20 +1081,18 @@ class haupt_Screen_Wall(Screen, ConfigListScreen):
 		if self.selektor_index > 1: 
 			self.selektor_index -= 1
 			self.move_selector()
-			print self.selektor_index
 			
 	def	keyRight(self):
 		if self.selektor_index < 40 and self.selektor_index != len(self.mainlist[int(self.select_list)]):
 			self.selektor_index += 1
 			self.move_selector()
-			print self.selektor_index
 			
 	def keyUp(self):
 		if self.selektor_index-8 > 1:
 			self.selektor_index -=8
 			self.move_selector()
 		else:
-			self.selektor_index = self.mainlist[int(self.select_list)][0]
+			self.selektor_index = 1
 			self.move_selector()
 
 	def keyDown(self):
@@ -1095,9 +1100,33 @@ class haupt_Screen_Wall(Screen, ConfigListScreen):
 			self.selektor_index +=8
 			self.move_selector()
 		else:
-			self.selektor_index = self.mainlist[int(self.select_list)][-1]
+			self.selektor_index = len(self.mainlist[int(self.select_list)])
 			self.move_selector()
 			
+	def page_next(self):
+		if self.select_list < len(self.mainlist)-1:
+			self.paint_hide()
+			self.select_list += 1
+			self.paint_new()
+	
+	def page_back(self):
+		if self.select_list > 0:
+			self.select_list -= 1
+			self.paint_hide()
+			self.paint_new()
+	
+	def paint_hide(self):
+		for x in self.mainlist[int(self.select_list)]:
+			self["zeile"+str(x)].hide()
+	
+	def paint_new(self):
+		pageinfo = "%s / %s" % (self.select_list+1, len(self.mainlist))
+		self['page'].setText(pageinfo)
+		self.selektor_index = 1
+		self.move_selector()
+		for x in self.mainlist[int(self.select_list)]:
+			self["zeile"+str(x)].show()
+	
 	def keySetup(self):
 		print config.mediaportal.pincode.value
 		self.session.openWithCallback(self.pinok, PinInput, pinList = [(config.mediaportal.pincode.value)], triesEntry = self.getTriesEntry(), title = _("Please enter the correct pin code"), windowTitle = _("Enter pin code"))
