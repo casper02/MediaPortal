bb69294881d90a00bb5bfb80dae692dbb7deabea
dhwz <dhwz@gulli.com>
2013-03-14 23:36:06 +0100

    updated xHamster

--- xHamster.py
+++ xHamster.py
@@ -5,6 +5,11 @@ def xhamsterGenreListEntry(entry):
 		(eListboxPythonMultiContent.TYPE_TEXT, 20, 0, 900, 25, 0, RT_HALIGN_CENTER | RT_VALIGN_CENTER, entry[0])
 		] 
 
+def xhamsterstreamListEntry(entry):
+	return [entry,
+		(eListboxPythonMultiContent.TYPE_TEXT, 20, 0, 900, 25, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, entry[0])
+		] 
+
 class xhamsterGenreScreen(Screen):
 	
 	def __init__(self, session):
@@ -20,13 +25,18 @@ class xhamsterGenreScreen(Screen):
 		Screen.__init__(self, session)
 		
 		self["actions"]  = ActionMap(["OkCancelActions", "ShortcutActions", "WizardActions", "ColorActions", "SetupActions", "NumberActions", "MenuActions"], {
-			"ok"    : self.keyOK,
-			"cancel": self.keyCancel
+			"ok" : self.keyOK,
+			"cancel": self.keyCancel,
+			"up" : self.keyUp,
+			"down" : self.keyDown,
+			"right" : self.keyRight,
+			"left" : self.keyLeft			
 		}, -1)
 		
 		self['title'] = Label("xHamster.com")
 		self['name'] = Label("Genre Auswahl")
 		self['coverArt'] = Pixmap()
+		self.keyLocked = True
 		self.suchString = ''
 		
 		self.genreliste = []
@@ -38,104 +48,55 @@ class xhamsterGenreScreen(Screen):
 		self.onLayoutFinish.append(self.layoutFinished)
 		
 	def layoutFinished(self):
-		self.genreliste.append(("--- Search ---", "callSuchen"))
-		self.genreliste.append(("New", "http://www.xhamster.com/new/"))
-		self.genreliste.append(("HD Videos", "http://www.xhamster.com/channels/new-hd_videos-"))
-		self.genreliste.append(("Amateur", "http://www.xhamster.com/channels/new-amateur-"))
-		self.genreliste.append(("Anal", "http://www.xhamster.com/channels/new-anal-"))
-		self.genreliste.append(("Arab", "http://www.xhamster.com/channels/new-arab-"))
-		self.genreliste.append(("Asian", "http://www.xhamster.com/channels/new-asian-"))
-		self.genreliste.append(("Babes", "http://www.xhamster.com/channels/new-babes-"))
-		self.genreliste.append(("Babysitters", "http://www.xhamster.com/channels/new-babysitters-"))
-		self.genreliste.append(("BBW", "http://www.xhamster.com/channels/new-bbw-"))
-		self.genreliste.append(("BDSM", "http://www.xhamster.com/channels/new-bdsm-"))
-		self.genreliste.append(("Beach", "http://www.xhamster.com/channels/new-beach-"))
-		self.genreliste.append(("Big Boobs", "http://www.xhamster.com/channels/new-big_boobs-"))
-		self.genreliste.append(("Bisexuals", "http://www.xhamster.com/channels/new-bisexuals-"))
-		self.genreliste.append(("Black and Ebony", "http://www.xhamster.com/channels/new-ebony-"))
-		self.genreliste.append(("Black Gays", "http://www.xhamster.com/channels/new-black_gays-"))
-		self.genreliste.append(("Blondes", "http://www.xhamster.com/channels/new-blondes-"))
-		self.genreliste.append(("Blowjobs", "http://www.xhamster.com/channels/new-blowjobs-"))
-		self.genreliste.append(("Brazilian", "http://www.xhamster.com/channels/new-brazilian-"))
-		self.genreliste.append(("British", "http://www.xhamster.com/channels/new-british-"))
-		self.genreliste.append(("Brunettes", "http://www.xhamster.com/channels/new-brunettes-"))
-		self.genreliste.append(("Bukkake", "http://www.xhamster.com/channels/new-bukkake-"))
-		self.genreliste.append(("Cartoons", "http://www.xhamster.com/channels/new-cartoons-"))
-		self.genreliste.append(("Celebrities", "http://www.xhamster.com/channels/new-celebs-"))
-		self.genreliste.append(("Chinese", "http://www.xhamster.com/channels/new-chinese-"))
-		self.genreliste.append(("Close-ups", "http://www.xhamster.com/channels/new-close_ups-"))
-		self.genreliste.append(("Cream Pie", "http://www.xhamster.com/channels/new-creampie-"))
-		self.genreliste.append(("Cuckold", "http://www.xhamster.com/channels/new-cuckold-"))
-		self.genreliste.append(("Cumshots", "http://www.xhamster.com/channels/new-cumshots-"))
-		self.genreliste.append(("Czech", "http://www.xhamster.com/channels/new-czech-"))
-		self.genreliste.append(("Danish", "http://www.xhamster.com/channels/new-danish-"))
-		self.genreliste.append(("Double Penetration", "http://www.xhamster.com/channels/new-double_penetration-"))
-		self.genreliste.append(("Emo", "http://www.xhamster.com/channels/new-emo-"))
-		self.genreliste.append(("Face Sitting", "http://www.xhamster.com/channels/new-face_sitting-"))
-		self.genreliste.append(("Facials", "http://www.xhamster.com/channels/new-facials-"))
-		self.genreliste.append(("Female Choice", "http://www.xhamster.com/channels/new-female_choice-"))
-		self.genreliste.append(("Femdom", "http://www.xhamster.com/channels/new-femdom-"))
-		self.genreliste.append(("Fingering", "http://www.xhamster.com/channels/new-fingering-"))
-		self.genreliste.append(("Flashing", "http://www.xhamster.com/channels/new-flashing-"))
-		self.genreliste.append(("Foot Fetish", "http://www.xhamster.com/channels/new-foot-fetish-"))
-		self.genreliste.append(("French", "http://www.xhamster.com/channels/new-french-"))
-		self.genreliste.append(("Funny", "http://www.xhamster.com/channels/new-funny-"))
-		self.genreliste.append(("Gangbang", "http://www.xhamster.com/channels/new-gangbang-"))
-		self.genreliste.append(("Gaping", "http://www.xhamster.com/channels/new-gaping-"))
-		self.genreliste.append(("Gays", "http://www.xhamster.com/channels/new-gays-"))
-		self.genreliste.append(("German", "http://www.xhamster.com/channels/new-german-"))
-		self.genreliste.append(("Gothic", "http://www.xhamster.com/channels/new-gothic-"))
-		self.genreliste.append(("Grannies", "http://www.xhamster.com/channels/new-grannies-"))
-		self.genreliste.append(("Group Sex", "http://www.xhamster.com/channels/new-group-"))
-		self.genreliste.append(("Hairy", "http://www.xhamster.com/channels/new-hairy-"))
-		self.genreliste.append(("Handjobs", "http://www.xhamster.com/channels/new-handjobs-"))
-		self.genreliste.append(("Hardcore", "http://www.xhamster.com/channels/new-hardcore-"))
-		self.genreliste.append(("Hentai", "http://www.xhamster.com/channels/new-hentai-"))
-		self.genreliste.append(("Hidden Cams", "http://www.xhamster.com/channels/new-hidden-"))
-		self.genreliste.append(("Hits", "http://www.xhamster.com/channels/new-hits-"))
-		self.genreliste.append(("Indian", "http://www.xhamster.com/channels/new-indian-"))
-		self.genreliste.append(("Interracial", "http://www.xhamster.com/channels/new-interracial-"))
-		self.genreliste.append(("Italian", "http://www.xhamster.com/channels/new-italian-"))
-		self.genreliste.append(("Japanese", "http://www.xhamster.com/channels/new-japanese-"))
-		self.genreliste.append(("Korean", "http://www.xhamster.com/channels/new-korean-"))
-		self.genreliste.append(("Ladyboys", "http://www.xhamster.com/channels/new-ladyboys-"))
-		self.genreliste.append(("Latex", "http://www.xhamster.com/channels/new-latex-"))
-		self.genreliste.append(("Latin", "http://www.xhamster.com/channels/new-latin-"))
-		self.genreliste.append(("Lesbians", "http://www.xhamster.com/channels/new-lesbians-"))
-		self.genreliste.append(("Lingerie", "http://www.xhamster.com/channels/new-lingerie-"))
-		self.genreliste.append(("Massage", "http://www.xhamster.com/channels/new-massage-"))
-		self.genreliste.append(("Masturbation", "http://www.xhamster.com/channels/new-masturbation-"))
-		self.genreliste.append(("Matures", "http://www.xhamster.com/channels/new-matures-"))
-		self.genreliste.append(("Men", "http://www.xhamster.com/channels/new-men-"))
-		self.genreliste.append(("Midgets", "http://www.xhamster.com/channels/new-midgets-"))
-		self.genreliste.append(("MILFs", "http://www.xhamster.com/channels/new-milfs-"))
-		self.genreliste.append(("Nipples", "http://www.xhamster.com/channels/new-nipples-"))
-		self.genreliste.append(("Old+Young", "http://www.xhamster.com/channels/new-old_young-"))
-		self.genreliste.append(("Pornstars", "http://www.xhamster.com/channels/new-pornstars-"))
-		self.genreliste.append(("POV", "http://www.xhamster.com/channels/new-pov-"))
-		self.genreliste.append(("Public Nudity", "http://www.xhamster.com/channels/new-public-"))
-		self.genreliste.append(("Redheads", "http://www.xhamster.com/channels/new-redheads-"))
-		self.genreliste.append(("Russian", "http://www.xhamster.com/channels/new-russian-"))
-		self.genreliste.append(("Sex Toys", "http://www.xhamster.com/channels/new-toys-"))
-		self.genreliste.append(("Shemales", "http://www.xhamster.com/channels/new-shemales-"))
-		self.genreliste.append(("Showers", "http://www.xhamster.com/channels/new-showers-"))
-		self.genreliste.append(("Softcore", "http://www.xhamster.com/channels/new-softcore-"))
-		self.genreliste.append(("Spanking", "http://www.xhamster.com/channels/new-spanking-"))
-		self.genreliste.append(("Squirting", "http://www.xhamster.com/channels/new-squirting-"))
-		self.genreliste.append(("Stockings", "http://www.xhamster.com/channels/new-stockings-"))
-		self.genreliste.append(("Strapon", "http://www.xhamster.com/channels/new-strapon-"))
-		self.genreliste.append(("Swedish", "http://www.xhamster.com/channels/new-swedish-"))
-		self.genreliste.append(("Swingers", "http://www.xhamster.com/channels/new-swingers-"))
-		self.genreliste.append(("Teens", "http://www.xhamster.com/channels/new-teens-"))
-		self.genreliste.append(("Thai", "http://www.xhamster.com/channels/new-tai-"))
-		self.genreliste.append(("Threesomes", "http://www.xhamster.com/channels/new-threesomes-"))
-		self.genreliste.append(("Tits", "http://www.xhamster.com/channels/new-tits-"))
-		self.genreliste.append(("Turkish", "http://www.xhamster.com/channels/new-turkish-"))
-		self.genreliste.append(("Upskirts", "http://www.xhamster.com/channels/new-upskirts-"))
-		self.genreliste.append(("Vintage", "http://www.xhamster.com/channels/new-vintage-"))
-		self.genreliste.append(("Voyeur", "http://www.xhamster.com/channels/new-voyeur-"))
-		self.genreliste.append(("Webcams", "http://www.xhamster.com/channels/new-webcams-"))
-		self.chooseMenuList.setList(map(xhamsterGenreListEntry, self.genreliste))
+		self.keyLocked = True
+		url = "http://www.xhamster.com/channels.php"
+		getPage(url, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.genreData).addErrback(self.dataError)
+
+	def genreData(self, data):
+		phCats = re.findall('<td>.*?<a href="(.*?)1.html">.*?<div align="center">.*?<img src="(.*?)".*?align="center">(.*?) \(.*?\)</div>', data, re.S)
+		if phCats:
+			for (phUrl, phImage, phTitle) in phCats:
+				phUrl = "http://www.xhamster.com" + phUrl
+				self.genreliste.append((phTitle, phUrl, phImage))
+			self.genreliste.sort()
+			self.genreliste.insert(0, ("Top Rated", "http://www.xhamster.com/channels/new-hits-", None))
+			self.genreliste.insert(0, ("--- Search ---", "callSuchen", None))
+			self.chooseMenuList.setList(map(xhamsterGenreListEntry, self.genreliste))
+			self.keyLocked = False
+			self.showInfos()
+
+	def dataError(self, error):
+		print error
+
+	def showInfos(self):
+		phImage = self['genreList'].getCurrent()[0][2]
+		print phImage
+		if not phImage == None:
+			downloadPage(phImage, "/tmp/xhIcon.jpg").addCallback(self.ShowCover)
+		else:
+			self.ShowCoverNone()
+
+	def ShowCover(self, picData):
+		picPath = "/tmp/xhIcon.jpg"
+		self.ShowCoverFile(picPath)
+		
+	def ShowCoverNone(self):
+		picPath = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/%s/images/no_coverArt.png" % config.mediaportal.skin.value
+		self.ShowCoverFile(picPath)
+		
+	def ShowCoverFile(self, picPath):
+		if fileExists(picPath):
+			self['coverArt'].instance.setPixmap(None)
+			self.scale = AVSwitch().getFramebufferScale()
+			self.picload = ePicLoad()
+			size = self['coverArt'].instance.size()
+			self.picload.setPara((size.width(), size.height(), self.scale[0], self.scale[1], False, 1, "#FF000000"))
+			if self.picload.startDecode(picPath, 0, 0, False) == 0:
+				ptr = self.picload.getData()
+				if ptr != None:
+					self['coverArt'].instance.setPixmap(ptr.__deref__())
+					self['coverArt'].show()
+					del self.picload
 
 	def keyOK(self):
 		streamGenreName = self['genreList'].getCurrent()[0][0]
@@ -144,7 +105,7 @@ class xhamsterGenreScreen(Screen):
 
 		else:
 			streamGenreLink = self['genreList'].getCurrent()[0][1]
-			self.session.open(xhamster, streamGenreLink)
+			self.session.open(xhamsterFilmScreen, streamGenreLink)
 		
 	def suchen(self):
 		self.session.openWithCallback(self.SuchenCallback, VirtualKeyBoard, title = (_("Suchkriterium eingeben")), text = self.suchString)
@@ -153,24 +114,43 @@ class xhamsterGenreScreen(Screen):
 		if callback is not None and len(callback):
 			self.suchString = callback.replace(' ', '+')
 			streamGenreLink = 'http://www.xhamster.com/search.php?q=%s&page=' % (self.suchString)
-			self.session.open(xhamster, streamGenreLink)
+			self.session.open(xhamsterFilmScreen, streamGenreLink)
+
+	def keyLeft(self):
+		if self.keyLocked:
+			return
+		self['genreList'].pageUp()
+		self.showInfos()
+		
+	def keyRight(self):
+		if self.keyLocked:
+			return
+		self['genreList'].pageDown()
+		self.showInfos()
+		
+	def keyUp(self):
+		if self.keyLocked:
+			return
+		self['genreList'].up()
+		self.showInfos()
+		
+	def keyDown(self):
+		if self.keyLocked:
+			return
+		self['genreList'].down()
+		self.showInfos()
 
 	def keyCancel(self):
 		self.close()
 
-def xhamsterstreamListEntry(entry):
-	return [entry,
-		(eListboxPythonMultiContent.TYPE_TEXT, 20, 0, 900, 25, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, entry[0])
-		] 
-
-class xhamster(Screen):
+class xhamsterFilmScreen(Screen):
 	
 	def __init__(self, session, genreLink):
 		self.session = session
 		self.genreLink = genreLink
-		path = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/%s/xhamster.xml" % config.mediaportal.skin.value
+		path = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/%s/xhamsterFilmScreen.xml" % config.mediaportal.skin.value
 		if not fileExists(path):
-			path = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/original/xhamster.xml"
+			path = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/original/xhamsterFilmScreen.xml"
 		print path
 		with open(path, "r") as f:
 			self.skin = f.read()
@@ -191,20 +171,19 @@ class xhamster(Screen):
 		}, -1)
 		
 		self['title'] = Label("xHamster.com")
-		self['coverArt'] = Pixmap()
-		self['name'] = Label("")
+		self['name'] = Label("Film Auswahl")
 		self['views'] = Label("")
 		self['runtime'] = Label("")
 		self['page'] = Label("1")
+		self['coverArt'] = Pixmap()
+		self.keyLocked = True
+		self.page = 1
 		
 		self.streamList = []
 		self.streamMenuList = MenuList([], enableWrapAround=True, content=eListboxPythonMultiContent)
 		self.streamMenuList.l.setFont(0, gFont('mediaportal', 23))
 		self.streamMenuList.l.setItemHeight(25)
 		self['streamlist'] = self.streamMenuList
-
-		self.keyLocked = False
-		self.page = 1
 
 		self.onLayoutFinish.append(self.loadpage)
 		
@@ -218,18 +197,17 @@ class xhamster(Screen):
 		getPage(ptUrl, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.pageData).addErrback(self.dataError)
 		
 	def pageData(self, data):
-		xhListe = re.findall('<a href="(/movies/.*?)" class=\'hRotator\' >.*?<img src=\'(.*?)\'.*?alt="(.*?)"/>.*?<div class="moduleFeaturedDetails">Runtime: (.*?)<BR>Views: (.*?)</div>', data, re.S)
+		parse = re.search('<h2>.*?</h2>.*?<h2>.*?</h2>(.*?)</table>', data, re.S)
+		xhListe = re.findall('<a href="(/movies/.*?)" class=\'hRotator\' >.*?<img src=\'(.*?)\'.*?alt="(.*?)"/>.*?<div class="moduleFeaturedDetails">Runtime: (.*?)<BR>Views: (.*?)</div>', parse.group(1), re.S)
 		if xhListe:
 			for (xhLink, xhImage, xhName, xhRuntime, xhxhViews) in xhListe:
-				xhLink = "http://xhamster.com"+xhLink
+				xhLink = "http://xhamster.com" + xhLink
 				self.streamList.append((decodeHtml(xhName), xhImage, xhLink, xhxhViews, xhRuntime))
 			self.streamMenuList.setList(map(xhamsterstreamListEntry, self.streamList))
 			self.showInfos()
 		self.keyLocked = False
 
 	def showInfos(self):
-		if self.keyLocked:
-			return
 		ptTitle = self['streamlist'].getCurrent()[0][0]
 		ptImage = self['streamlist'].getCurrent()[0][1]
 		ptViews  = self['streamlist'].getCurrent()[0][3]
