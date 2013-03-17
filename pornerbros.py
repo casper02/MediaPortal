b64068f7f0afeedbac46e323346dc9066c1dfd47
dhwz <dhwz@gulli.com>
2013-03-17 20:10:41 +0100

    updated pornerbros

--- pornerbros.py
+++ pornerbros.py
@@ -48,67 +48,60 @@ class pornerbrosGenreScreen(Screen):
 		self.onLayoutFinish.append(self.layoutFinished)
 		
 	def layoutFinished(self):
-		self.genreliste.append(("--- Search ---", "callSuchen"))
-		self.genreliste.append(("New", "http://www.pornerbros.com/page"))
-		self.genreliste.append(("Top Rated", "http://www.pornerbros.com/best/page"))
-		self.genreliste.append(("Most Viewed", "http://www.pornerbros.com/popular/page"))
-		self.genreliste.append(("Most Discussed", "http://www.pornerbros.com/discussed/page"))
-		self.genreliste.append(("Longest", "http://www.pornerbros.com/longest/page"))
-		self.genreliste.append(("Creampie", "http://www.pornerbros.com/category/creampie/page"))
-		self.genreliste.append(("Cuckolding", "http://www.pornerbros.com/category/cuckolding/page"))
-		self.genreliste.append(("Cumshot", "http://www.pornerbros.com/category/cumshot/page"))
-		self.genreliste.append(("Double Penetration", "http://www.pornerbros.com/category/dp/page"))
-		self.genreliste.append(("Drunk Girls", "http://www.pornerbros.com/category/drunk-girls/page"))
-		self.genreliste.append(("Ebony", "http://www.pornerbros.com/category/ebony/page"))
-		self.genreliste.append(("Euro", "http://www.pornerbros.com/category/european/page"))
-		self.genreliste.append(("Fat Girls", "http://www.pornerbros.com/category/fat/page"))
-		self.genreliste.append(("Female Friendly", "http://www.pornerbros.com/category/female-friendly/page"))
-		self.genreliste.append(("Femdom", "http://www.pornerbros.com/category/femdom/page"))
-		self.genreliste.append(("Fetish", "http://www.pornerbros.com/category/fetish/page"))
-		self.genreliste.append(("Fisting", "http://www.pornerbros.com/category/fisting/page"))
-		self.genreliste.append(("Gangbang", "http://www.pornerbros.com/category/gang-bang/page"))
-		self.genreliste.append(("Gay", "http://www.pornerbros.com/category/gay/page"))
-		self.genreliste.append(("Girlfriend", "http://www.pornerbros.com/category/exgf/page"))
-		self.genreliste.append(("Granny", "http://www.pornerbros.com/category/granny/page"))
-		self.genreliste.append(("Group", "http://www.pornerbros.com/category/group-sex/page"))
-		self.genreliste.append(("Hairy Pussy", "http://www.pornerbros.com/category/hairy/page"))
-		self.genreliste.append(("Handjob", "http://www.pornerbros.com/category/handjob/page"))
-		self.genreliste.append(("Hardcore", "http://www.pornerbros.com/category/hardcore/page"))
-		self.genreliste.append(("Hentai", "http://www.pornerbros.com/category/hentai/page"))
-		self.genreliste.append(("High Definition", "http://www.pornerbros.com/category/hd/page"))
-		self.genreliste.append(("Interracial", "http://www.pornerbros.com/category/interracial/page"))
-		self.genreliste.append(("Latina", "http://www.pornerbros.com/category/latina/page"))
-		self.genreliste.append(("Lesbian", "http://www.pornerbros.com/category/lesbians/page"))
-		self.genreliste.append(("Massage", "http://www.pornerbros.com/category/massage/page"))
-		self.genreliste.append(("Masturbation", "http://www.pornerbros.com/category/masturbation/page"))
-		self.genreliste.append(("Mature", "http://www.pornerbros.com/category/mature/page"))
-		self.genreliste.append(("Midget", "http://www.pornerbros.com/category/midget/page"))
-		self.genreliste.append(("MILF", "http://www.pornerbros.com/category/milf/page"))
-		self.genreliste.append(("Office", "http://www.pornerbros.com/category/office-sex/page"))
-		self.genreliste.append(("Old & Young", "http://www.pornerbros.com/category/old-young/page"))
-		self.genreliste.append(("Orgy", "http://www.pornerbros.com/category/orgy/page"))
-		self.genreliste.append(("Outdoor", "http://www.pornerbros.com/category/outdoor-sex/page"))
-		self.genreliste.append(("POV", "http://www.pornerbros.com/category/pov/page"))
-		self.genreliste.append(("Pissing", "http://www.pornerbros.com/category/pissing/page"))
-		self.genreliste.append(("Pornstars", "http://www.pornerbros.com/category/pornstars/page"))
-		self.genreliste.append(("Pregnant", "http://www.pornerbros.com/category/pregnant/page"))
-		self.genreliste.append(("Public", "http://www.pornerbros.com/category/public-sex/page"))
-		self.genreliste.append(("Pussy", "http://www.pornerbros.com/category/pussy/page"))
-		self.genreliste.append(("Redhead", "http://www.pornerbros.com/category/redhead/page"))
-		self.genreliste.append(("Sex Party", "http://www.pornerbros.com/category/sex-party/page"))
-		self.genreliste.append(("Shemale", "http://www.pornerbros.com/category/shemale/page"))
-		self.genreliste.append(("Solo", "http://www.pornerbros.com/category/solo/page"))
-		self.genreliste.append(("Squirting", "http://www.pornerbros.com/category/squirting/page"))
-		self.genreliste.append(("Stockings", "http://www.pornerbros.com/category /category/stockings/page"))
-		self.genreliste.append(("Teen", "http://www.pornerbros.com/category /category/teens/page"))
-		self.genreliste.append(("Threesome", "http://www.pornerbros.com/category/threesome/page"))
-		self.genreliste.append(("Toys", "http://www.pornerbros.com/category/toys/page"))
-		self.genreliste.append(("Uniform", "http://www.pornerbros.com/category/uniform/page"))
-		self.genreliste.append(("Vintage", "http://www.pornerbros.com/category/vintage/page"))
-		self.genreliste.append(("Voyeur", "http://www.pornerbros.com/category/voyeur/page"))
-		self.genreliste.append(("Webcam", "http://www.pornerbros.com/category/webcam/page"))
-		self.chooseMenuList.setList(map(pornerbrosGenreListEntry, self.genreliste))
+		self.keyLocked = True
+		url = "http://www.pornerbros.com/categories/"
+		getPage(url, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.genreData).addErrback(self.dataError)
+
+	def genreData(self, data):
+		phCats = re.findall('class="contents_gallery_item">.*?<a href="(.*?)" title="(.*?)">.*?<img alt=.*?data-original="(.*?)" onClick', data, re.S)
+		if phCats:
+			for (phUrl, phTitle, phImage) in phCats:
+				phUrl = "http://www.pornerbros.com" + phUrl + 'page'
+				self.genreliste.append((phTitle, phUrl, phImage))
+			self.genreliste.sort()
+			self.genreliste.insert(0, ("Longest", "http://www.pornerbros.com/longest/page", None))
+			self.genreliste.insert(0, ("Most Discussed", "http://www.pornerbros.com/discussed/page", None))
+			self.genreliste.insert(0, ("Most Viewed", "http://www.pornerbros.com/popular/page", None))
+			self.genreliste.insert(0, ("Top Rated", "http://www.pornerbros.com/best/page", None))
+			self.genreliste.insert(0, ("New", "http://www.pornerbros.com/page", None))
+			self.genreliste.insert(0, ("--- Search ---", "callSuchen", None))
+			self.chooseMenuList.setList(map(pornerbrosGenreListEntry, self.genreliste))
+			self.keyLocked = False
+			self.showInfos()	
+
+	def dataError(self, error):
+		print error
+
+	def showInfos(self):
+		phImage = self['genreList'].getCurrent()[0][2]
+		print phImage
+		if not phImage == None:
+			downloadPage(phImage, "/tmp/phIcon.jpg").addCallback(self.ShowCover)
+		else:
+			self.ShowCoverNone()
+
+	def ShowCover(self, picData):
+		picPath = "/tmp/phIcon.jpg"
+		self.ShowCoverFile(picPath)
 		
+	def ShowCoverNone(self):
+		picPath = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/%s/images/no_coverArt.png" % config.mediaportal.skin.value
+		self.ShowCoverFile(picPath)
+		
+	def ShowCoverFile(self, picPath):
+		if fileExists(picPath):
+			self['coverArt'].instance.setPixmap(None)
+			self.scale = AVSwitch().getFramebufferScale()
+			self.picload = ePicLoad()
+			size = self['coverArt'].instance.size()
+			self.picload.setPara((size.width(), size.height(), self.scale[0], self.scale[1], False, 1, "#FF000000"))
+			if self.picload.startDecode(picPath, 0, 0, False) == 0:
+				ptr = self.picload.getData()
+				if ptr != None:
+					self['coverArt'].instance.setPixmap(ptr.__deref__())
+					self['coverArt'].show()
+					del self.picload
+
 	def keyOK(self):
 		streamGenreName = self['genreList'].getCurrent()[0][0]
 		if streamGenreName == "--- Search ---":
@@ -128,16 +121,28 @@ class pornerbrosGenreScreen(Screen):
 			self.session.open(pornerbrosFilmScreen, streamGenreLink)
 
 	def keyLeft(self):
+		if self.keyLocked:
+			return
 		self['genreList'].pageUp()
+		self.showInfos()
 		
 	def keyRight(self):
+		if self.keyLocked:
+			return
 		self['genreList'].pageDown()
+		self.showInfos()
 		
 	def keyUp(self):
+		if self.keyLocked:
+			return
 		self['genreList'].up()
+		self.showInfos()
 		
 	def keyDown(self):
+		if self.keyLocked:
+			return
 		self['genreList'].down()
+		self.showInfos()
 
 	def keyCancel(self):
 		self.close()
@@ -169,7 +174,7 @@ class pornerbrosFilmScreen(Screen):
 			"green" : self.keyPageNumber
 		}, -1)
 		
-		self['title'] = Label("Pornbros.com")
+		self['title'] = Label("Pornerbros.com")
 		self['name'] = Label("Film Auswahl")
 		self['views'] = Label("")
 		self['runtime'] = Label("")
@@ -191,12 +196,20 @@ class pornerbrosFilmScreen(Screen):
 		self['name'].setText('Bitte warten...')
 		self.streamList = []
 		self['page'].setText(str(self.page))
-		url = "%s%s/" % (self.phCatLink, str(self.page))
+		cat = self.phCatLink
+		search = re.search('/search/(.*)', cat, re.S)
+		if search:
+			url = '%s%s' % (self.phCatLink, str(self.page))
+		else:
+			url = "%s%s/" % (self.phCatLink, str(self.page))
 		print url
 		getPage(url, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.pageData).addErrback(self.dataError)
 
 	def pageData(self, data):
-		xhListe = re.findall('<div class="contents_gallery_item">.*?<a href="(.*?)" title="(.*?)".*?gid="(.*?)".*?data-original="(.*?.jpg)".*?length">(.*?)<.*?views">(.*?)</span>', data, re.S)
+		parse = re.search('<h2>Today(.*)', data, re.S)
+		if not parse:
+			parse = re.search('<h2>Videos found(.*)', data, re.S)
+		xhListe = re.findall('<div class="contents_gallery_item">.*?<a href="(.*?)" title="(.*?)".*?gid="(.*?)".*?data-original="(.*?.jpg)".*?length">(.*?)<.*?views">(.*?)</span>', parse.group(1), re.S)
 		if xhListe:
 			for (xhLink, xhName, xhIdnr, xhImage, xhRuntime, xhViews) in xhListe:
 				self.streamList.append((decodeHtml(xhName), xhLink, xhIdnr, xhImage, xhRuntime, xhViews))
@@ -205,8 +218,6 @@ class pornerbrosFilmScreen(Screen):
 		self.keyLocked = False
 
 	def showInfos(self):
-		if self.keyLocked:
-			return
 		ptTitle = self['genreList'].getCurrent()[0][0]
 		ptImage = self['genreList'].getCurrent()[0][3]
 		ptRuntime = self['genreList'].getCurrent()[0][4]
