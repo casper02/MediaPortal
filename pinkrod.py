d0c36ae64e60b04fa43a981bd67c33ac28e2a861
dhwz <dhwz@gulli.com>
2013-03-17 10:51:26 +0100

    Added 3 new HD-only porn portals: Pinkrod.com, WetPlace.com, TheNewPorn.com

---
+++ pinkrod.py
@@ -0,0 +1,308 @@
+from imports import *
+
+def pinkrodGenreListEntry(entry):
+	return [entry,
+		(eListboxPythonMultiContent.TYPE_TEXT, 20, 0, 900, 25, 0, RT_HALIGN_CENTER | RT_VALIGN_CENTER, entry[0])
+		] 
+
+def pinkrodFilmListEntry(entry):
+	return [entry,
+		(eListboxPythonMultiContent.TYPE_TEXT, 20, 0, 900, 25, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, entry[0])
+		] 
+		
+class pinkrodGenreScreen(Screen):
+	
+	def __init__(self, session):
+		self.session = session
+		path = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/%s/pinkrodGenreScreen.xml" % config.mediaportal.skin.value
+		if not fileExists(path):
+			path = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/original/pinkrodGenreScreen.xml"
+		print path
+		with open(path, "r") as f:
+			self.skin = f.read()
+			f.close()
+			
+		Screen.__init__(self, session)
+		
+		self["actions"]  = ActionMap(["OkCancelActions", "ShortcutActions", "WizardActions", "ColorActions", "SetupActions", "NumberActions", "MenuActions", "EPGSelectActions"], {
+			"ok" : self.keyOK,
+			"cancel" : self.keyCancel,
+			"up" : self.keyUp,
+			"down" : self.keyDown,
+			"right" : self.keyRight,
+			"left" : self.keyLeft
+		}, -1)
+
+		self['title'] = Label("Pinkrod.com")
+		self['name'] = Label("Genre Auswahl")
+		self['coverArt'] = Pixmap()
+		self.keyLocked = True
+		self.suchString = ''
+		
+		self.genreliste = []
+		self.chooseMenuList = MenuList([], enableWrapAround=True, content=eListboxPythonMultiContent)
+		self.chooseMenuList.l.setFont(0, gFont('mediaportal', 23))
+		self.chooseMenuList.l.setItemHeight(25)
+		self['genreList'] = self.chooseMenuList
+		
+		self.onLayoutFinish.append(self.layoutFinished)
+		
+	def layoutFinished(self):
+		self.keyLocked = True
+		url = "http://www.pinkrod.com/categories/"
+		getPage(url, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.genreData).addErrback(self.dataError)
+
+	def genreData(self, data):
+		parse = re.search('class="category">(.*?)categories_block', data, re.S)
+		phCats = re.findall('<img class="thumb" src="(.*?)" alt="(.*?)"/>.*?<div class="title"><a href="(.*?)"', parse.group(1), re.S)
+		if phCats:
+			for (phImage, phTitle, phUrl) in phCats:
+				self.genreliste.append((phTitle, phUrl, phImage))
+			self.genreliste.sort()
+			self.genreliste.insert(0, ("Most Popular", "http://www.pinkrod.com/most-popular/", None))
+			self.genreliste.insert(0, ("Top Rated", "http://www.pinkrod.com/top-rated/", None))
+			self.genreliste.insert(0, ("Newest", "http://www.pinkrod.com/", None))
+			self.genreliste.insert(0, ("--- Search ---", "callSuchen", None))
+			self.chooseMenuList.setList(map(pinkrodGenreListEntry, self.genreliste))
+			self.keyLocked = False
+			self.showInfos()
+
+	def dataError(self, error):
+		print error
+
+	def showInfos(self):
+		phImage = self['genreList'].getCurrent()[0][2]
+		print phImage
+		if not phImage == None:
+			downloadPage(phImage, "/tmp/phIcon.jpg").addCallback(self.ShowCover)
+		else:
+			self.ShowCoverNone()
+
+	def ShowCover(self, picData):
+		picPath = "/tmp/phIcon.jpg"
+		self.ShowCoverFile(picPath)
+		
+	def ShowCoverNone(self):
+		picPath = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/%s/images/no_coverArt.png" % config.mediaportal.skin.value
+		self.ShowCoverFile(picPath)
+		
+	def ShowCoverFile(self, picPath):
+		if fileExists(picPath):
+			self['coverArt'].instance.setPixmap(None)
+			self.scale = AVSwitch().getFramebufferScale()
+			self.picload = ePicLoad()
+			size = self['coverArt'].instance.size()
+			self.picload.setPara((size.width(), size.height(), self.scale[0], self.scale[1], False, 1, "#FF000000"))
+			if self.picload.startDecode(picPath, 0, 0, False) == 0:
+				ptr = self.picload.getData()
+				if ptr != None:
+					self['coverArt'].instance.setPixmap(ptr.__deref__())
+					self['coverArt'].show()
+					del self.picload
+
+	def keyOK(self):
+		streamGenreName = self['genreList'].getCurrent()[0][0]
+		if streamGenreName == "--- Search ---":
+			self.suchen()
+
+		else:
+			streamGenreLink = self['genreList'].getCurrent()[0][1]
+			self.session.open(pinkrodFilmScreen, streamGenreLink)
+		
+	def suchen(self):
+		self.session.openWithCallback(self.SuchenCallback, VirtualKeyBoard, title = (_("Suchkriterium eingeben")), text = self.suchString)
+
+	def SuchenCallback(self, callback = None, entry = None):
+		if callback is not None and len(callback):
+			self.suchString = callback.replace(' ', '+')
+			streamGenreLink = 'http://www.pinkrod.com/search/?q=%s' % self.suchString
+			self.session.open(pinkrodFilmScreen, streamGenreLink)
+
+	def keyLeft(self):
+		if self.keyLocked:
+			return
+		self['genreList'].pageUp()
+		self.showInfos()
+		
+	def keyRight(self):
+		if self.keyLocked:
+			return
+		self['genreList'].pageDown()
+		self.showInfos()
+		
+	def keyUp(self):
+		if self.keyLocked:
+			return
+		self['genreList'].up()
+		self.showInfos()
+		
+	def keyDown(self):
+		if self.keyLocked:
+			return
+		self['genreList'].down()
+		self.showInfos()
+
+	def keyCancel(self):
+		self.close()
+
+class pinkrodFilmScreen(Screen):
+	
+	def __init__(self, session, phCatLink):
+		self.session = session
+		self.phCatLink = phCatLink
+		path = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/%s/pinkrodFilmScreen.xml" % config.mediaportal.skin.value
+		if not fileExists(path):
+			path = "/usr/lib/enigma2/python/Plugins/Extensions/mediaportal/skins/original/pinkrodFilmScreen.xml"
+		print path
+		with open(path, "r") as f:
+			self.skin = f.read()
+			f.close()
+			
+		Screen.__init__(self, session)
+		
+		self["actions"]  = ActionMap(["OkCancelActions", "ShortcutActions", "WizardActions", "ColorActions", "SetupActions", "NumberActions", "MenuActions", "EPGSelectActions"], {
+			"ok" : self.keyOK,
+			"cancel" : self.keyCancel,
+			"up" : self.keyUp,
+			"down" : self.keyDown,
+			"right" : self.keyRight,
+			"left" : self.keyLeft,
+			"nextBouquet" : self.keyPageUp,
+			"prevBouquet" : self.keyPageDown,
+			"green" : self.keyPageNumber
+		}, -1)
+
+		self['title'] = Label("Pinkrod.com")
+		self['name'] = Label("Film Auswahl")
+		self['views'] = Label("")
+		self['runtime'] = Label("")
+		self['page'] = Label("1")
+		self['coverArt'] = Pixmap()
+		self.keyLocked = True
+		self.page = 1
+		
+		self.filmliste = []
+		self.chooseMenuList = MenuList([], enableWrapAround=True, content=eListboxPythonMultiContent)
+		self.chooseMenuList.l.setFont(0, gFont('mediaportal', 23))
+		self.chooseMenuList.l.setItemHeight(25)
+		self['genreList'] = self.chooseMenuList
+		
+		self.onLayoutFinish.append(self.loadpage)
+		
+	def loadpage(self):
+		self.keyLocked = True
+		self['name'].setText('Bitte warten...')
+		self.filmliste = []
+		self['page'].setText(str(self.page))
+		cat = self.phCatLink
+		search = re.search('/search/(.*)', cat, re.S)
+		if search:
+			url = 'http://www.pinkrod.com/search/%s/%s' % (str(self.page), str(search.group(1)))
+		else:
+			url = "%s%s/" % (self.phCatLink, str(self.page))
+		print url
+		getPage(url, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.loadData).addErrback(self.dataError)
+	
+	def loadData(self, data):
+		phMovies = re.findall('class="video">.*?<a href="(.*?)".*?title="(.*?)"><img class="thumb" src="(.*?)"', data, re.S)
+		if phMovies:
+			for (phUrl, phTitle, phImage) in phMovies:
+				self.filmliste.append((decodeHtml(phTitle), phUrl, phImage))
+			self.chooseMenuList.setList(map(pinkrodFilmListEntry, self.filmliste))
+			self.showInfos()
+		self.keyLocked = False
+
+	def dataError(self, error):
+		print error
+
+	def showInfos(self):
+		phTitle = self['genreList'].getCurrent()[0][0]
+		phImage = self['genreList'].getCurrent()[0][2]
+		self['name'].setText(phTitle)
+		downloadPage(phImage, "/tmp/Icon.jpg").addCallback(self.ShowCover)
+		
+	def ShowCover(self, picData):
+		if fileExists("/tmp/Icon.jpg"):
+			self['coverArt'].instance.setPixmap(None)
+			self.scale = AVSwitch().getFramebufferScale()
+			self.picload = ePicLoad()
+			size = self['coverArt'].instance.size()
+			self.picload.setPara((size.width(), size.height(), self.scale[0], self.scale[1], False, 1, "#FF000000"))
+			if self.picload.startDecode("/tmp/Icon.jpg", 0, 0, False) == 0:
+				ptr = self.picload.getData()
+				if ptr != None:
+					self['coverArt'].instance.setPixmap(ptr.__deref__())
+					self['coverArt'].show()
+					del self.picload
+
+	def keyPageNumber(self):
+		self.session.openWithCallback(self.callbackkeyPageNumber, VirtualKeyBoard, title = (_("Seitennummer eingeben")), text = str(self.page))
+
+	def callbackkeyPageNumber(self, answer):
+		if answer is not None:
+			self.page = int(answer)
+			self.loadpage()
+
+	def keyPageDown(self):
+		print "PageDown"
+		if self.keyLocked:
+			return
+		if not self.page < 2:
+			self.page -= 1
+			self.loadpage()
+		
+	def keyPageUp(self):
+		print "PageUP"
+		if self.keyLocked:
+			return
+		self.page += 1
+		self.loadpage()
+		
+	def keyLeft(self):
+		if self.keyLocked:
+			return
+		self['genreList'].pageUp()
+		self.showInfos()
+		
+	def keyRight(self):
+		if self.keyLocked:
+			return
+		self['genreList'].pageDown()
+		self.showInfos()
+		
+	def keyUp(self):
+		if self.keyLocked:
+			return
+		self['genreList'].up()
+		self.showInfos()
+		
+	def keyDown(self):
+		if self.keyLocked:
+			return
+		self['genreList'].down()
+		self.showInfos()
+		
+	def keyOK(self):
+		if self.keyLocked:
+			return
+		phTitle = self['genreList'].getCurrent()[0][0]
+		phLink = self['genreList'].getCurrent()[0][1]
+		self.keyLocked = True
+		getPage(phLink, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.getVideoPage).addErrback(self.dataError)
+
+	def getVideoPage(self, data):
+		videoPage = re.findall('<source type="video/mp4" src="(.*?)">', data, re.S)
+		if videoPage:
+			for phurl in videoPage:
+				url = phurl
+				self.keyLocked = False
+				self.play(url)
+		
+	def play(self,file):
+		xxxtitle = self['genreList'].getCurrent()[0][0]
+		sref = eServiceReference(0x1001, 0, file)
+		sref.setName(xxxtitle)
+		self.session.open(MoviePlayer, sref)
+
+	def keyCancel(self):
+		self.close()
