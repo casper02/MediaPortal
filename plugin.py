ebbb2c26879e8b812a329c4a0e29e6c09b4affe2
einfall <martin8984@web.de>
2013-04-24 13:51:10 +0200

    fix

--- plugin.py
+++ plugin.py
@@ -700,11 +700,7 @@ class haupt_Screen(Screen, ConfigListScreen):
 			self.porn.append(self.hauptListEntry("YouPorn", "youporn"))
 		
 		if len(self.porn) < 1:
-<<<<<<< HEAD
 			self['Porn'].hide()
-=======
-			self['Porn'] = Label("")
->>>>>>> origin/einfall
 
 		self.mediatheken.sort(key=lambda t : tuple(t[0][0].lower()))
 		self.grauzone.sort(key=lambda t : tuple(t[0][0].lower()))
