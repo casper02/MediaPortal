e6a9f30eedd5fb0ff34da23bec563503beb1e1c3
dhwz <dhwz@gulli.com>
2013-04-25 16:58:03 +0200

    raise version number to 4.2.2

--- plugin.py
+++ plugin.py
@@ -102,8 +102,8 @@ from additions.porn.x4tube import *
 from additions.porn.youporn import *
 
 config.mediaportal = ConfigSubsection()
-config.mediaportal.version = NoSave(ConfigText(default="421"))
-config.mediaportal.versiontext = NoSave(ConfigText(default="4.2.1"))
+config.mediaportal.version = NoSave(ConfigText(default="422"))
+config.mediaportal.versiontext = NoSave(ConfigText(default="4.2.2"))
 config.mediaportal.autoupdate = ConfigYesNo(default = True)
 config.mediaportal.pincode = ConfigPIN(default = 0000)
 config.mediaportal.skin = ConfigSelection(default = "tec", choices = [("tec", _("tec")),("liquidblue", _("liquidblue")), ("original", _("original"))])
